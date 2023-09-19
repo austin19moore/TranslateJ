d7652b186524c03e8ae173e421d68fae82458f15
Austin Moore <austin19moore@gmail.com>
2023-09-18 22:25:04 -0500

    can now directly type into transcription window, enter to translate

--- src/main/java/application/Controller/MainController.java
+++ src/main/java/application/Controller/MainController.java
@@ -8,6 +8,7 @@ import application.Model.syncSpeech;
 import com.deepl.api.TextResult;
 import com.deepl.api.Translator;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,6 +16,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 
 import javax.sound.sampled.*;
@@ -43,9 +46,29 @@ public class MainController implements Initializable {
     private static boolean recording = false;
     private Record recAudio;
 
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         setTargetLine(getMic());
+        cancelButton.setDisable(true);
+
+        transcriptionArea.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                String text = "";
+                if (event.getCode() == KeyCode.ENTER) {
+                    try {
+                        text = deeplTranslate(transcriptionArea.getText());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    if (!text.equals("")) {
+                        translationArea.setText(text);
+                    }
+                }
+            }
+        });
 
         try {
             SettingsController.getSettings();
@@ -62,28 +85,33 @@ public class MainController implements Initializable {
     public void startButton(ActionEvent arg0) throws Exception {
 
         recAudio = new Record();
-        //cancelButton.setVisible(true);
-        cancelButton.setDisable(false);
 
         if (recording) {
             recAudio.stop(targetLine);
-            startButton.setText("Stop");
+            startButton.setText("Start");
+            recording = false;
+            cancelButton.setDisable(true);
 
             System.out.println("Starting transcription...");
             application.Model.syncSpeech syncT = new syncSpeech();
             String transcription = syncT.transcribe("src/main/resources/audio/recording.wav", SettingsController.getSourceLanguage());
-            // String transcription = "ただ様で始める前にさちょっとね昨日ね愛おしいねファミリーベッド混じる小学校のジル君の写真をねあの送ってねやつぶやいたのみんなに見てもらいたくてつぶやいたらさ";
 
             System.out.println("Starting translation...");
-            String translation = deeplTranslate(transcription);
+            String translation = "";
+
+            if (transcription.equals("") || transcription.isEmpty()) {
+                translation = "Error: Transcription was empty.";
+            } else {
+                translation = deeplTranslate(transcription);
+            }
 
             transcriptionArea.setText(transcription);
             translationArea.setText(translation);
-            startButton.setText("Start");
             System.out.println("done");
 
         } else {
             recording = true;
+            cancelButton.setDisable(false);
             Record.getAudio(targetLine);
             System.out.println("Starting record...");
             startButton.setText("Stop");
@@ -91,23 +119,24 @@ public class MainController implements Initializable {
 
     }
 
+    // translate string to from source language to target language
     public String deeplTranslate(String transcription) throws Exception {
         Translator translator = new Translator(SettingsController.getAuthKey());
         TextResult result = translator.translateText(transcription, SettingsController.getSourceLanguage(), SettingsController.getTargetLanguage());
         System.out.println("translation: " + result.getText());
         return result.getText();
-
     }
 
+    // cancel, don't transcribe/translate
     public void cancelButton(ActionEvent arg0) {
-
+        System.out.println("Cancelling...");
         recAudio.stop(targetLine);
         recording = false;
         startButton.setText("Start");
         cancelButton.setDisable(true);
-
     }
 
+    // switch user to settings view
     public void settingsButton(ActionEvent arg0) {
 
         try {
@@ -121,20 +150,19 @@ public class MainController implements Initializable {
 
     }
 
+    // attempt to swap from default mic to stereo mix
     public void swapButton(ActionEvent arg0) throws Exception {
-
         if (mixer != null && mixer.getMixerInfo().getName().contains("Stereo Mix")) {
             setTargetLine(getMic());
-            System.out.println("mic");
+            System.out.println("Switched to: Default Mic");
         } else {
             setTargetLine(getStereo());
-            System.out.println(mixer.getMixerInfo().getName());
+            System.out.println("Switched to: " + mixer.getMixerInfo().getName());
         }
-
     }
 
+    // get default mic
     public TargetDataLine getMic() {
-
         AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 48000, 16, 2, 4, 48000, false);
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
         TargetDataLine line = null;
@@ -148,11 +176,10 @@ public class MainController implements Initializable {
 
         mixer = null;
         return line;
-
     }
 
+    // attempt to get stereo mixer
     public TargetDataLine getStereo() throws LineUnavailableException {
-
         Mixer.Info[] mixers = AudioSystem.getMixerInfo();
         AudioFormat audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 48000, 16, 2, 4, 48000, false);
         DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
@@ -166,9 +193,9 @@ public class MainController implements Initializable {
 
         TargetDataLine line = (TargetDataLine) mixer.getLine(dataLineInfo);
         return line;
-
     }
 
+    // setters/getters
     public static void setTargetLine(TargetDataLine tLine) {
         targetLine = tLine;
     }
